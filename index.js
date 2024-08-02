0115950381e64a7398f6ffb5a0098bb848aa19e0
hamzakhan512 <hamzakh620@gmail.com>
2024-08-02 09:13:28 +0500

    Add js file

---
+++ index.js
@@ -0,0 +1,16 @@
+let country = "Pakistan";
+let continent = "Asia";
+let population = "235.8 Million";
+
+console.log(country)
+console.log(continent)
+console.log(population)
+
+let isIsland = "false";
+let language;
+
+console.log(isIsland)
+console.log(population)
+console.log(country)
+console.log(language)
+
